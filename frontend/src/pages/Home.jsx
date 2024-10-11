9bf76c40c4a2b12430df6a3c4142a7118752a67c
dharshini-out-println <1vijayadharshini@gmail.com>
2024-10-11 16:08:15 +0530

    updated

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -7,6 +7,8 @@ import heroVideo from '../assets/images/hero-video.mp4';
 import Subtitle from '../shared/Subtitle';
 import worldImg from '../assets/images/world.png';
 import SearchBar from '../shared/SearchBar';
+import ServiceList from "../services/ServiceList";
+import FeaturedTourList from '../components/Featured-tours/FeaturedTourList';
 
 const Home = () => {
   return (
@@ -61,6 +63,31 @@ const Home = () => {
         </Container>
       </section>
       {/*================= hero section end ===============*/}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='3'>
+            <h5 className="services__subtitle">What we serve</h5>
+            <h2 className='services__title'>We offer our best services</h2>
+            </Col>
+            <ServiceList />
+          </Row>
+        </Container>
+      </section>
+
+      {/*===============featured tour section start================ */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg='12' className="mb-5">
+            <Subtitle subtitle={'Explore'}/>
+            <h2 className="featured__tour-title">Our featured tours</h2>
+            </Col>
+            <FeaturedTourList />
+          </Row>
+        </Container>
+      </section>
+      {/*===============featured tour section end================ */}
     </>
   );
 };
