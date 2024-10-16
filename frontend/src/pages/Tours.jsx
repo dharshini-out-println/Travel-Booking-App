4fe1ac03bc972ce0c46730187fd6c351408fb9da
dharshini-out-println <1vijayadharshini@gmail.com>
2024-10-16 20:12:58 +0530

    updated

--- frontend/src/pages/Tours.jsx
+++ frontend/src/pages/Tours.jsx
@@ -1,9 +1,25 @@
-import React from 'react'
+import React from 'react';
+import CommonSection from '../shared/CommonSection';
+import '../styles/tour.css';
+import TourCard from './../shared/TourCard';
+import SearchBar from './../shared/SearchBar';
+import NewsLetter from './../shared/Newsletter';
+import tourData from '../assets/data/tours';
+import { Container,Row } from 'reactstrap';
 
 const Tours = () => {
   return (
-    <div>Tours</div>
-  )
-}
+    <>
+    <CommonSection title={'All tours'}/>
+    <section>
+      <Container>
+        <Row>
+          <SearchBar/>
+        </Row>
+      </Container>
+    </section>
+    </>
+  );
+};
 
 export default Tours
