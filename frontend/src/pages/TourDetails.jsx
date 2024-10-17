a9d8f8c604a68c03ae7cd7db6f276a04abeef14d
dharshini-out-println <1vijayadharshini@gmail.com>
2024-10-17 18:37:09 +0530

    updated

--- frontend/src/pages/TourDetails.jsx
+++ frontend/src/pages/TourDetails.jsx
@@ -25,11 +25,47 @@ const TourDetails = () => {
             {totalRating===0?(
               "Not rated"
             ):(
-              <span>({reviews.length})</span>
+              <span>({reviews?.length})</span>
             )}
           </span>
+           <span>
+            <i className="ri-map-pin-user-fill"></i>{address}
+           </span>
               </div>
+              <div className="tour__extra-details">
+                <span>
+                  <i className="ri-map-pin-2-line"></i> {city}
+                </span>
+                <span>
+                  <i className="ri-money-dollar-circle-line"></i> ${price} /person
+                </span>
+                <span>
+                  <i className="ri-group-line"></i> {maxGroupSize}
+                </span>
+              </div>
+              <h5>Description</h5>
+              <p>{desc}</p>
             </div>
+            {/* -------------tour reviews section-------------- */}
+            <div className="tour__reviews mt-4">
+              <h4>Reviews ({reviews?.length} reviews)</h4>
+              <Form>
+                <div className="d-flex align-items-center gap-3 mb-4 rating__group">
+                     1 <span><i className="ri-star-s-fill"></i></span>
+                     2 <span><i className="ri-star-s-fill"></i></span>
+                     3 <span><i className="ri-star-s-fill"></i></span>
+                     4 <span><i className="ri-star-s-fill"></i></span>
+                     5 <span><i className="ri-star-s-fill"></i></span>
+                </div>
+                <div className="review__input">
+                  <input type="text" placeholder='share your thoughts' />
+                  <button className='btn primary__btn text-white' type="submit">
+                    Submit
+                  </button>
+                </div>
+              </Form>
+            </div>
+            {/* -------------tour reviews section-------------- */}
           </div>
           </Col>
         </Row>
